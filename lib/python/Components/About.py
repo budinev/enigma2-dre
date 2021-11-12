1ff10f8e0b2878d75bd1c2013d26883934dd58f0
jack2015 <344784320@qq.com>
2021-11-12 13:23:35 -0800

    update About.py

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -4,7 +4,8 @@ import os
 import time
 import re
 from Tools.HardwareInfo import HardwareInfo
-
+from boxbranding import getBoxType
+boxtype = getBoxType()
 
 def getFlashMemory(folder='/'):
 	try:
@@ -177,7 +178,10 @@ def getDriverInstalledDate():
 		from glob import glob
 		try:
 			with open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") as fp:
-				driver = [x.split("-")[-2:-1][0][-8:] for x in fp if x.startswith("Version:")][0]
+				if boxtype in ("dm800", "dm8000"):
+					driver = [x.split("-")[-2:-1][0][-9:] for x in fp if x.startswith("Version:")][0]
+				else:
+					driver = [x.split("-")[-2:-1][0][-8:] for x in fp if x.startswith("Version:")][0]
 				return "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
 		except:
 			try:
