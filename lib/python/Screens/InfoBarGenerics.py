7daebb4760ea1d0d553a1dc86f06e2c179cb866d
jack2015 <344784320@qq.com>
2022-01-01 17:29:37 +0800

    temp

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -112,8 +112,8 @@ def saveResumePoints():
 	global resumePointCache, resumePointCacheLast
 	import cPickle
 	try:
-		with open('/home/root/resumepoints.pkl', 'wb') as f:
-			cPickle.dump(resumePointCache, f, cPickle.HIGHEST_PROTOCOL)
+		f = open('/etc/enigma2/resumepoints.pkl', 'wb')
+		cPickle.dump(resumePointCache, f, cPickle.HIGHEST_PROTOCOL)
 	except Exception, ex:
 		print "[InfoBar] Failed to write resumepoints:", ex
 	resumePointCacheLast = int(time())
@@ -122,12 +122,7 @@ def saveResumePoints():
 def loadResumePoints():
 	import cPickle
 	try:
-		if os.path.isfile('/home/root/resumepoints.pkl'):
-			with open('/home/root/resumepoints.pkl', 'rb') as f:
-				return cPickle.load(f)
-		else:
-			print "[InfoBar] resumepoints is lost"
-			return {}
+		return cPickle.load(open('/etc/enigma2/resumepoints.pkl', 'rb'))
 	except Exception, ex:
 		print "[InfoBar] Failed to load resumepoints:", ex
 		return {}
@@ -142,11 +137,7 @@ class whitelist:
 
 
 def reload_whitelist_vbi():
-	if os.path.isfile('/etc/enigma2/whitelist_vbi'):
-		with open('/etc/enigma2/whitelist_vbi', 'r') as f:
-			whitelist.vbi = [line.strip() for line in f.readlines()]
-	else:
-		whitelist.vbi = []
+	whitelist.vbi = [line.strip() for line in open('/etc/enigma2/whitelist_vbi', 'r').readlines()] if os.path.isfile('/etc/enigma2/whitelist_vbi') else []
 
 
 reload_whitelist_vbi()
@@ -162,8 +153,7 @@ def reload_subservice_groupslist(force=False):
 			groupedservices = "/etc/enigma2/groupedservices"
 			if not os.path.isfile(groupedservices):
 				groupedservices = "/usr/share/enigma2/groupedservices"
-			with open(groupedservices) as fp:
-				subservice.groupslist = [list(g) for k, g in itertools.groupby([line.split('#')[0].strip() for line in fp.readlines()], lambda x:not x) if not k]
+			subservice.groupslist = [list(g) for k, g in itertools.groupby([line.split('#')[0].strip() for line in open(groupedservices).readlines()], lambda x:not x) if not k]
 		except:
 			subservice.groupslist = []
 
@@ -474,8 +464,7 @@ class InfoBarShowHide(InfoBarScreenSaver):
 				whitelist.vbi.remove(service)
 			else:
 				whitelist.vbi.append(service)
-			with open('/etc/enigma2/whitelist_vbi', 'w') as f:
-				f.write('\n'.join(whitelist.vbi))
+			open('/etc/enigma2/whitelist_vbi', 'w').write('\n'.join(whitelist.vbi))
 			self.showHideVBI()
 
 
@@ -2082,8 +2071,7 @@ class InfoBarTimeshift():
 
 	def setLCDsymbolTimeshift(self):
 		if SystemInfo["LCDsymbol_timeshift"]:
-			with open(SystemInfo["LCDsymbol_timeshift"], "w") as fp:
-				fp.write(self.timeshiftEnabled() and "1" or "0")
+			open(SystemInfo["LCDsymbol_timeshift"], "w").write(self.timeshiftEnabled() and "1" or "0")
 
 	def __serviceStarted(self):
 		self.pvrStateDialog.hide()
@@ -3622,6 +3610,7 @@ class InfoBarPowersaver:
 			print "[InfoBarPowersaver] goto standby"
 			self.session.open(Screens.Standby.Standby)
 
+
 class InfoBarHDMI:
 	def HDMIIn(self):
 		slist = self.servicelist
@@ -3641,37 +3630,3 @@ class InfoBarHDMI:
 				self.session.nav.playService(slist.servicelist.getCurrent())
 			else:
 				self.session.nav.playService(self.cur_service)
-
-class InfoBarHDMI2:
-	def __init__(self):
-		self.hdmi_enabled_input = False
-		if SystemInfo["DMHDMI"]:
-			if not self.hdmi_enabled_input:
-				self.addExtension((self.getHDMIInputScreen, self.HDMIInput, lambda: True), "green")
-
-	def getHDMIInputScreen(self):
-		if not self.hdmi_enabled_input:
-			return _("Turn on HDMI-IN mode")
-		else:
-			return _("Turn off HDMI-IN mode")
-
-	def HDMIInput(self):
-			f = open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor", "r")
-			check = f.read()
-			f.close()
-			if check.startswith("off"):
-				f = open("/proc/stb/audio/hdmi_rx_monitor", "w")
-				f.write("on")
-				f.close()
-				f = open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor", "w")
-				f.write("on")
-				f.close()
-				self.hdmi_enabled_input = True
-			else:
-				f = open("/proc/stb/audio/hdmi_rx_monitor", "w")
-				f.write("off")
-				f.close()
-				f = open("/proc/stb/hdmi-rx/0/hdmi_rx_monitor", "w")
-				f.write("off")
-				f.close()
-				self.hdmi_enabled_input = False
